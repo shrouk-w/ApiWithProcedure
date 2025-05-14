f9661b81d44af7873fcf3f04a80c867f7817b477
Kacper-Wierzba <kacper.wierzbaa@wp.pl>
2025-05-14 11:30:10 +0200

    added structure and made 1st controller

---
+++ WebApplication1/WebApplication1/Controllers/WareHouseController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApplication1.DTOs;
+
+namespace WebApplication1.Controllers;
+
+
+
+[ApiController]
+[Route("api/[controller]")]
+public class WareHouseController: ControllerBase
+{
+    
+    private readonly ITripService _tripService;
+
+    public WareHouseController(ITripService tripService)
+    {
+        _tripService = tripService;
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> AddProduct(AddProductDTO product)
+    {
+        var response = await _tripService.AddProduct(product);
+        return Ok();
+    }
+    
+    
+}
---
+++ WebApplication1/WebApplication1/DTOs/AddProductDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebApplication1.DTOs;
+
+public class AddProductDTO
+{
+    [Required]
+    int IdProduct{ get; set; }
+    [Required]
+    int IdWarehouse{ get; set; }
+    [Required]
+    int Amount{ get; set; }
+}
